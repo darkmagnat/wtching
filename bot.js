962e7a199721556de462f750f32104928ce0995e
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-12 19:12:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,22 @@
-﻿const Discord = require("discord.js");
+const Discord = require('discord.js')
 const client = new Discord.Client();
+client.on('ready', function(){
+ console.log(`Logged in as ${client.user.tag}!`);
+   
+    client.user.setPresence({
+ game: { 
+    type:'WATCHING',
+     url: 'WATCHING',
+    name: '-Universe Town` 1K-Soon........ ',
+    application_id: '501452003143319553',//هنا ايدي البوت 
+     assets: {
+         large_image:   `509420418453471236`, //هنا id صوره يلي جبت
+  
+    }
+  }
+    });
+  
+});
 
 
-client.on('ready', () => {
-    client.user.setGame(`-Universe Town`,'https://www.twitch.tv/v5bz');
-console.log("Discord Bot By So.?");
-  console.log('BySo.?')
-  console.log('BySo.?')
-  console.log('BySo.?')
-  console.log('BySo.?')
-  console.log('BySo.?')
-
-
-}); 
-
-client.login(process.env.BOT_TOKEN); 
+client.login(process.env.BOT_TOKEN);
